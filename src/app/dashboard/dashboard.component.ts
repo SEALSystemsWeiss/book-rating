d0324917a741eea0672974b65af24d95b45c07ff
SEALSystemsWeiss <weissch-111>
2018-07-10 14:10:40 +0200

    added dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: string[]; // Array<string>
+
+  ngOnInit() {
+    this.books = ['Angular', 'AngularJS', 'Perl'];
+  }
+
+}
