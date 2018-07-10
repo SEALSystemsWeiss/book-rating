4b313e03db663180f6bc1971f5e15dfe5d1f85ec
SEALSystemsWeiss <weissch-111>
2018-07-10 15:14:52 +0200

    Unittest NO_ERRORS_SCHEMA

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 describe('AppComponent', () => {
 
   beforeEach(async(() => {
@@ -10,7 +11,10 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent, DashboardComponent
+        AppComponent//, DashboardComponent
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
       ],
     }).compileComponents();
   }));
