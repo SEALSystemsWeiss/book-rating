d11bfe8935fe4938e96565b63cedf6a6d4ff4ece
SEALSystemsWeiss <weissch-111>
2018-07-11 16:56:50 +0200

    my first BBD Test

--- src/app/book/book.component.spec.ts
+++ src/app/book/book.component.spec.ts
@@ -8,8 +8,9 @@ describe('BookComponent', () => {
   let fixture: ComponentFixture<BookComponent>;
 
   // smalllest possible Mock
+  let hasBeenCalled = false;
   const ratingMock = {
-    rateUp: () => { },
+    rateUp: () => hasBeenCalled = true,
   };
 
   beforeEach(async(() => {
@@ -37,7 +38,8 @@ describe('BookComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should forward the rateUp call to the servcie', () => {
+    component.rateUp();
+    expect(hasBeenCalled).toBe(true);
   });
 });
