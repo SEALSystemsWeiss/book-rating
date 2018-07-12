51b112ec50646f69c0cb346e797cbffbc263659b
SEALSystemsWeiss <weissch-111>
2018-07-12 16:01:07 +0200

    Observable, added dummy book on error

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Book } from './book';
+import { filter, retry, catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,15 @@ export class BookStoreService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>('http://api.angular.schule/books');
+    return this.http.get<Book[]>('http://api.angular.schule/books')
+      .pipe(
+        retry(3),
+        catchError(() => of([{
+          isbn: '000',
+          title: 'test on error',
+          description: 'Sorry',
+          rating: 5
+        }]))
+      );
   }
-
-
 }
