249becfd79472e28960608403d27e37e219e1d0a
SEALSystemsWeiss <weissch-111>
2018-07-12 09:57:45 +0200

    e2e test extern URL

---
+++ e2e/src/angular.e2e-spec.ts
@@ -0,0 +1,15 @@
+import { browser, element, by, $ } from 'protractor';
+describe('angular book', () => {
+
+  beforeAll(() => browser.waitForAngularEnabled(false));
+
+  it('should just call it Angular', () => {
+
+    browser.get('https://www.dpunkt.de/buecher/12400/9783864903571-angular.html');
+    // const heading = element(by.tagName('h3'));
+    const heading = $('h1');
+    expect(heading.getText()).toEqual('Angular'); // Rot erwartet
+  });
+
+  afterAll(() => browser.waitForAngularEnabled(false));
+});
