83bcbc96662d6dc3cd8874368879271960949e19
SEALSystemsWeiss <weissch-111>
2018-07-13 13:26:06 +0200

    moved all book-stuff to Module books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { BookComponent } from './book/book.component';
-import { CreateBookComponent } from './create-book/create-book.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { BookDetailsComponent } from './book-details/book-details.component';
+
+import { BooksModule } from './books/books.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    BookComponent,
-    CreateBookComponent,
-    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
+    BooksModule,  // Reihenfolge wichtig!!!!!
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookComponent } from './book/book.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 import { BooksRoutingModule } from './books-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    ReactiveFormsModule,
+    // KEIN HttpClientModule !!
   ],
-  declarations: []
+  declarations: [
+    DashboardComponent,
+    BookComponent,
+    CreateBookComponent,
+    BookDetailsComponent
+  ]
 })
 export class BooksModule { }
